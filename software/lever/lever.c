8919b9cc8e09314207016f6452c302d604adf5c3
Andy Lustig <alustig3@gmail.com>
2020-02-04 17:45:11 -0500

    can adjust servo position by receiving signal on pyControl's DIO_C. configuration is loaded from and saved to EEPROM

--- software/lever/lever.c
+++ software/lever/lever.c
@@ -5,17 +5,38 @@
 #include <util/delay.h>
 #include <avr/power.h>
 #include <avr/interrupt.h>
+#include <avr/eeprom.h>
 
 #define duty_cycle OCR1B
+#define retract_default 1700
+#define extend_default 1250 
 
-uint16_t retracted_pos = 1800;
-uint16_t extended_pos = 1150;
+uint16_t *config_start_address  = (uint16_t *) 0;
+
+uint16_t retracted_pos;
+uint16_t extended_pos; 
 
 int main(void) {
   clock_prescale_set(clock_div_8); // set frequency to 1MHz
 
-  // Timer setup
-  TCCR1B |= (1<<CS10); // set prescale of 1=
+  //read data from EEPROM
+  retracted_pos = eeprom_read_word(config_start_address);
+  if (retracted_pos == 65535){
+    retracted_pos = retract_default;
+    eeprom_update_word(config_start_address, retracted_pos);
+  }
+  extended_pos = eeprom_read_word(config_start_address+1);
+  if (extended_pos == 65535){
+    extended_pos = extend_default;
+    eeprom_update_word(config_start_address+1, extended_pos);
+  }
+
+
+  // Timer 0 Setup
+  TCCR0B |= (1<<CS02) | (1<<CS00); // set timer frequency to clk/1024 ~ We can count from 0-261.12 ms with 1.024ms resolution
+
+  // PWM Timer setup
+  TCCR1B |= (1<<CS10); // set timer frequency to clk/1 ~ Each tick is 1 µs
   // Wave generation mode 14, using ICR1 as TOP
   TCCR1A |= (1<<WGM11);
   TCCR1B |= (1<<WGM13) | (1<<WGM12);
@@ -28,15 +49,32 @@ int main(void) {
 
 
   while (1) {
-    if ((PINA & (1<<PA4))){ //
+    if (PINA & (1<<PA4)){ //
       duty_cycle = extended_pos;
-      PORTA |= (1<<PA3);
+      if (PINB & (1<<PB2)){ // we're getting an adjustment signal
+        TCNT0 = 0;
+        PORTA |= (1<<PA3);
+        while(PINB & (1<<PB2)){
+        }
+        extended_pos = extend_default - TCNT0;
+        eeprom_update_word(config_start_address + 1, extended_pos);
+        PORTA &= ~(1<<PA3);
+      }
     }
     else{
       duty_cycle = retracted_pos;
-      PORTA &= ~(1<<PA3);
+      if (PINB & (1<<PB2)){ // we're getting an adjustment signal
+        TCNT0 = 0;
+        PORTA |= (1<<PA3);
+        while(PINB & (1<<PB2)){
+        }
+        retracted_pos = retract_default + TCNT0;
+        eeprom_update_word(config_start_address, retracted_pos);
+        PORTA &= ~(1<<PA3);
+      }
     }
     _delay_ms(1);
+
   }
 
 
